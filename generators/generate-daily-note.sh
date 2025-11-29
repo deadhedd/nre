f28b52391dae24a573caf9bd3ad9dd088d9adee7
deadhedd <167578248+deadhedd@users.noreply.github.com>
2025-11-28 18:01:36 -0800

    Update generate-daily-note.sh

--- generators/generate-daily-note.sh
+++ generators/generate-daily-note.sh
@@ -163,15 +163,17 @@ log_info "Generating note for date: $today"
 log_info "Preparing time block navigation"
 
 time_blocks_nav=$(cat <<EOF_TB
-## 🕑 Time Blocks
+
+## 🗺️ Navigation
+### 🕑 Time Blocks
  
  [[Periodic Notes/Daily Notes/Subnotes/2025-11-28 - Wake Up|Wake Up]] · [[Periodic Notes/Daily Notes/Subnotes/2025-11-28 - Morning|Morning]] · [[Periodic Notes/Daily Notes/Subnotes/2025-11-28 - Afternoon|Afternoon]] · [[Periodic Notes/Daily Notes/Subnotes/2025-11-28 - Evening|Evening]] · [[Periodic Notes/Daily Notes/Subnotes/2025-11-28 - Night|Night]]
 
-## 📅 Periodic Notes
+### 📅 Periodic Notes
 
 [[Periodic Notes/Weekly Notes/2025-W48|This Week]] · [[Periodic Notes/Monthly Notes/2025-11|This Month]] · [[Periodic Notes/Quarterly Notes/2025-Q4|This Quarter]] · [[Periodic Notes/Yearly Notes/2025|This Year]]
 
-##  🔗 Links
+###  🔗 Links
 
 [[Weekly Routine]] · [[Consider Johnie]] · [[Daily Note Template]] · [[Daily Plan]] · [[Workout Schedule]]
 EOF_TB
